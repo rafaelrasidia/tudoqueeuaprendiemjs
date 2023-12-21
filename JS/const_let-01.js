b592791d604f5dd13f78ca0ccd1bdd784e8f50f5
raphadev87 <raphaeldiasdev@gmail.com>
2023-12-21 16:27:15 -0300

    fin alizando revisão do código para postagem de vídeo

---
+++ JS/const_let-01.js
@@ -0,0 +1,8 @@
+// const não pode ter valor reatribuído.
+const eVerdade = "sim";
+let eMentira = "não"; // alterando para let, consego mudar o valor 
+
+if (eVerdade ==="sim") {
+    eMentira = "sim"; // agora consigo reatribuir valor à variável :)
+    console.log(eMentira)
+}
